c4666740c0484cd8b55243c19537fc1760d1a7bb
irfansyakir <irfansyakir@gmail.com>
2024-05-23 09:30:12 +0800

    test

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ const About = () => {
   return (
     <Main
       title="About"
-      description="Learn about Michael D'Angelo"
+      description="Learn about Irfan D'Angelo"
     >
       <article className="post markdown" id="about">
         <header>
